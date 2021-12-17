c454295cb1791e11845882648ead5c466fe13fad
khanh.nguyen <khanh.nguyen@shopee.com>
2021-12-17 23:09:26 +0800

    commit

--- app/Main.hs
+++ app/Main.hs
@@ -3,11 +3,14 @@ import Data.Sort (sortOn)
 import Solver (solve)
 import Parser (parse)
 import Debug.Trace (trace)
+import System.Posix.Internals (puts)
 
 
 main :: IO ()
 main = do
     f <- parse "test.cnf"
     let (sat, assignment) = solve f
-    print sat
-    print $ sortOn abs assignment
+    if not sat
+        then putStrLn "UNSAT"
+        else
+            putStrLn ("SAT\n" ++ unwords (map show (sortOn abs assignment)))
