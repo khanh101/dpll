aecab942fb127c5f0c4171a999dbb353a33420b5
khanh nguyen <khanh.nguyen.contact@gmail.com>
2023-04-21 02:06:46 +0800

    m

--- app/Solver.hs
+++ app/Solver.hs
@@ -20,26 +20,26 @@ scan (x:xs)
 
 
 
--- substitute : set literal to true and return the reduced formula
-substitute ::  Formula -> Literal -> Formula
-substitute [] _ = []
-substitute (x:xs) l
-    | l `elem` x    = substitute xs l  -- if l in the first clause, remove that clause, return substitution of the rest
-    | (-l) `elem` x = substitute (delete (-l) x : xs) l -- if (-l) in the first clause, remove the first (-l) in the clause, return substitution of the remaining
-    | otherwise     = x : substitute xs l -- if neither l or (-l) not in the first clause, return first clause ++ substitution of the rest
+-- subs : set literal to true and return the reduced formula
+subs ::  Literal -> Formula -> Formula
+subs _ [] = []
+subs l (x:xs)
+    | l `elem` x    = subs l xs  -- if l in the first clause, remove that clause, return substitution of the rest
+    | (-l) `elem` x = subs l (delete (-l) x : xs) -- if (-l) in the first clause, remove the first (-l) in the clause, return substitution of the remaining
+    | otherwise     = x : subs l xs -- if neither l or (-l) not in the first clause, return first clause ++ substitution of the rest
 
 -- solve : formula is sat
 solve :: Formula -> (Bool, Assignment)
-solve f = solveWithAssumption f []
+solve = solveWithAssumption []
 
 
 -- solveWithAssumption : solve a formula, push assignment to assumption and return
-solveWithAssumption :: Formula -> Assignment -> (Bool, Assignment)
-solveWithAssumption [] assumption = (True, assumption)
-solveWithAssumption formula assumption =
+solveWithAssumption :: Assignment -> Formula -> (Bool, Assignment)
+solveWithAssumption assumption [] = (True, assumption)
+solveWithAssumption assumption formula =
     case s of
         Unsat           -> (False, [])
-        Unit unit  -> solveWithAssumption (substitute formula unit) (unit:assumption)
+        Unit unit  -> solveWithAssumption (unit:assumption) (subs unit formula) 
         Guess
             | b1        -> (True, l1)
             | b2        -> (True, l2)
@@ -47,5 +47,5 @@ solveWithAssumption formula assumption =
     where 
         s = scan formula
         guess = head $ head formula
-        (b1, l1) = solveWithAssumption (substitute formula guess) (guess:assumption)
-        (b2, l2) = solveWithAssumption (substitute formula (-guess)) (-guess:assumption)
+        (b1, l1) = solveWithAssumption (guess:assumption) (subs guess formula) 
+        (b2, l2) = solveWithAssumption (-guess:assumption) (subs (-guess) formula) 
